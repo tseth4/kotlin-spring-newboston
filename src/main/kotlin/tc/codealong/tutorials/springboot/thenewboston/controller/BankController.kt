47ebf008432b885f0653b9083c2b51a081c3490f
ts <tristansetha@gmail.com>
2024-02-07 09:56:04 -0800

    get single bank finished

--- src/main/kotlin/tc/codealong/tutorials/springboot/thenewboston/controller/BankController.kt
+++ src/main/kotlin/tc/codealong/tutorials/springboot/thenewboston/controller/BankController.kt
@@ -1,5 +1,8 @@
 package tc.codealong.tutorials.springboot.thenewboston.controller
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +14,11 @@ import tc.codealong.tutorials.springboot.thenewboston.service.BankService
 @RestController
 @RequestMapping("api/banks")
 class BankController(private val service: BankService) {
+    // define exception handler, map internal exceptions that occured like No Such Element, map to appropraite status codes
+    // some type of internal exception that shouldnt be exposed to the outside
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
 
     @GetMapping
     fun getBanks(): Collection<Bank> = service.getBanks()
--- src/test/kotlin/tc/codealong/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
+++ src/test/kotlin/tc/codealong/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
@@ -1,7 +1,10 @@
 package tc.codealong.tutorials.springboot.thenewboston.controller
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,34 +19,61 @@ internal class BankControllerTest {
     // MockMvc class is typically used in Spring MVC testing to perform requests and validate responses without starting a full web server.
     @Autowired
     lateinit var mockMvc: MockMvc
+    val baseUrl = "/api/banks"
 
-    @Test
-    fun `should return all banks`() {
-        mockMvc.get("/api/banks")
-            .andDo { print() }
-            .andExpect {
-                status { isOk() }
-                content { contentType(MediaType.APPLICATION_JSON) }
-                jsonPath("$[0].accountNumber") {
-                    value("12345")
+    @Nested
+    @DisplayName("getBanks()")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class GetBanks {
+        @Test
+        fun `should return all banks`() {
+            mockMvc.get(baseUrl)
+                .andDo { print() }
+                .andExpect {
+                    status { isOk() }
+                    content { contentType(MediaType.APPLICATION_JSON) }
+                    jsonPath("$[0].accountNumber") {
+                        value("12345")
+                    }
                 }
-            }
+        }
     }
 
-    @Test
-    fun `should return bank with given account number`() {
-        val accountNumber = 1234
-        mockMvc.get("/api/banks/$accountNumber")
-            .andDo { print() }
-            .andExpect {
-                status { isOk() }
-                content { contentType(MediaType.APPLICATION_JSON) }
-                jsonPath("$.trust") {
-                    value("0.0")
+    @Nested
+    @DisplayName("getBank()")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class GetBank {
+        @Test
+        fun `should return bank with given account number`() {
+            val accountNumber = 1234
+            mockMvc.get("$baseUrl/$accountNumber")
+                .andDo { print() }
+                .andExpect {
+                    status { isOk() }
+                    content { contentType(MediaType.APPLICATION_JSON) }
+                    jsonPath("$.trust") {
+                        value("0.0")
+                    }
+                    jsonPath("$.transactionFee") {
+                        value("100")
+                    }
                 }
-                jsonPath("$.transactionFee") {
-                    value("100")
-                }
-            }
+        }
+
+        @Test
+        fun `should return Not Found if the account number does not exist`() {
+            // given
+            val accountNumber = "random_not_existing_account_number"
+            // when
+            mockMvc.get("$baseUrl/$accountNumber")
+                .andDo { print() }
+                .andExpect { status { isNotFound()} }
+
+            // then
+
+        }
+
     }
+
+
 }
